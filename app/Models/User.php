bdaed7e2d6bfaf576e946e514ff7c56315c2c7b1
Dzakky Budiman <dzakkybudiman19@gmail.com>
2024-09-09 21:27:50 +0700

    Add Databases Config

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function role(){
+        return $this->belongsTo(Role::class, 'role', 'id');
+    }
 }
