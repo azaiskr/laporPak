a3f9c0254c52e9e1c594eb867361dd9e42c40c55
Dhiya Urrahman <urrahmand0@gmail.com>
2024-09-10 14:13:05 +0700

    fix footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,79 +1,105 @@
 <footer class="lg:w-full">
     <div class="max-w-[1600px] mx-auto">
-        <div
-            class="z-10 box-border mx-[2rem] relative flex justify-between items-center bg-black text-white rounded-lg px-12 py-7">
+        <div class="z-10 box-border mx-40 relative flex justify-between items-center bg-black text-white rounded-lg px-12 py-7">
             <div>
-                <div>Need more information?</div>
+                <h2 class="text-4xl font-extrabold whitespace-nowrap mb-4">Need more information?</h2>
                 <div>Write your concern to us and our specialist will get back to you.</div>
             </div>
-            <div class="w-fit">
-                <button type="button" class="bg-white text-black px-5 py-2">Contact Us</button>
+            <div class="w-fit text-center">
+                <a href="#" class="inline-flex items-center justify-center px-8 py-4 text-2xl font-medium text-center text-black bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
+                    Button
+                </a> 
             </div>
         </div>
-        <div class="flex justify-evenly bg-[#F8F8F8] px-10 pt-20 pb-16 mt-[-3rem]">
-            <div>
-                <div>Logo</div>
-                <div>
-                    <div>
-                        <div>Address</div>
-                        <div>Level 1, 12 Sample St, Sydney NSW 2000</div>
+        <div>
+            <div class="flex justify-start items-start px-10 pt-20 pb-16 mt-[-3rem] mx-10 gap-14">
+                <div class="flex flex-col gap-8 sm:gap-6 sm:grid-row-3 text-black">
+                    <div class="text-2xl font-extrabold whitespace-nowrap">
+                        <span>Logo</span>
                     </div>
                     <div>
-                        <div>Contact</div>
-                        <div>1800 123 4567</div>
-                        <div>info@template.io</div>
+                        <h2 class="text-md font-bold whitespace-nowrap">Address:</h2>
+                        <span>Level 1, 12 Sample St, Sydney NSW 2000</span>
+                    </div>
+                    <div>
+                        <h2 class="text-md font-bold whitespace-nowrap">Contact:</h2>
+                        <p class="underline underline-offset-auto">1800 123 4567</p>
+                        <p class="underline underline-offset-auto">info@template.io</p>
+                    </div>
+                </div>
+                <div class="flex flex-row mt-4 justify-evenly mx-auto w-full">
+                    <div class="flex flex-row gap-32">
+                        <div>
+                            <h2 class="mb-6 text-xl font-semibold text-black">Heading</h2>
+                            <ul>
+                                <li>Link here</li>
+                                <li>Link here</li>
+                                <li>Link here</li>
+                                <li>Link here</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 class="mb-6 text-xl font-semibold text-black" >Heading</h2>
+                            <ul>
+                                <li>Link here</li>
+                                <li>Link here</li>
+                                <li>Link here</li>
+                                <li>Link here</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2 class="mb-6 text-xl font-semibold text-black" >Heading</h2>
+                            <ul>
+                                <li>Link here</li>
+                                <li>Link here</li>
+                                <li>Link here</li>
+                                <li>Link here</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        <h2 class="mb-6 text-xl font-semibold text-black" >Connect with us</h2>
+                        <div class="flex flex-row gap-4">
+                            <div>
+                                <img src="instagram.svg" alt="instagram">
+                            </div>
+                            <div>
+                                <img src="linkedin.svg" alt="linkedin">
+                            </div>
+                            <div>
+                                <img src="youtube.svg" alt="youtube">
+                            </div>
+                            <div>
+                                <img src="facebook.svg" alt="facebook">
+                            </div>
+                            <div>
+                                <img src="x.svg" alt="x">
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="flex gap-4">
-                <div>
-                    <div>Heading</div>
-                    <ul>
-                        <li>Link here</li>
-                        <li>Link here</li>
-                        <li>Link here</li>
-                        <li>Link here</li>
-                    </ul>
+            <div class="flex justify-between w-full max-w-screen px-10 py-5 mx-auto">
+                <div class="sm:flex sm:items-center sm:justify-between">
+                    <span class="text-sm text-black sm:text-center">© 2024 <a href="https://flowbite.com/" class="hover:underline"></a>. All Rights Reserved.
+                    </span>
                 </div>
-                <div>
-                    <div>Heading</div>
-                    <ul>
-                        <li>Link here</li>
-                        <li>Link here</li>
-                        <li>Link here</li>
-                        <li>Link here</li>
-                    </ul>
-                </div>
-                <div>
-                    <div>Heading</div>
-                    <ul>
-                        <li>Link here</li>
-                        <li>Link here</li>
-                        <li>Link here</li>
-                        <li>Link here</li>
-                    </ul>
-                </div>
-            </div>
-            <div>
-                <div>Connect with us</div>
-                <div class="flex flex-col">
-                    <div>
-                        <img src="x.svg" alt="x">
-                    </div>
-                    <div>
-                        <img src="facebook.svg" alt="facebook">
-                    </div>
-                    <div>
-                        <img src="instagram.svg" alt="instagram">
-                    </div>
-                    <div>
-                        <img src="youtube.svg" alt="youtube">
-                    </div>
-                    <div>
-                        <img src="linkedin.svg" alt="linkedin">
-                    </div>
-                </div>
+                          <div class="flex mt-4 sm:justify-center sm:mt-0">
+              <a href="#" class="underline underline-offset-auto text-black hover:text-gray-900">
+                  Privacy Policy
+              </a>
+              <a href="#" class="underline underline-offset-auto text-black hover:text-gray-900 ms-5">
+                  <span class="sr-only">Discord community</span>
+              </a>
+              <a href="#" class="underline underline-offset-auto text-black hover:text-gray-900 ms-5">
+                  Terms of Service
+              </a>
+              <a href="#" class="underline underline-offset-auto text-black hover:text-gray-900 ms-5">
+                  Cookie Settings
+              </a>
+          </div>
             </div>
         </div>
     </div>
 </footer>
+
