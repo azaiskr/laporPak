1d0b92a3c131e4ae28e5db9cf3ba3566f92562e0
Dhiya Urrahman <urrahmand0@gmail.com>
2024-09-10 20:19:22 +0700

    add lapor page
    create new lapor page, no js yet.
    - waiting dropdown design
    - dummy id names for inputs
    - dummy for names for labels

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,19 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/lapor', function () {
-        return view('lapor');
-    }) -> name('lapor');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+//     Route::get('/lapor', function () {
+//         return view('lapor');
+//     }) -> name('lapor');
+// });
+
+// Uncomment Later ^^^^
+
+Route::get('/lapor', function () {
+    return view('lapor');
+})->name('lapor');
 
 require __DIR__.'/auth.php';
