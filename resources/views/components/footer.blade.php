739ff9a1689f603b4c8ff4bd18f2f26843c30395
Noobmaster32 <athallacahyonugroho5@gmail.com>
2024-09-10 10:13:51 +0700

    save for pull

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,79 @@
+<footer class="lg:w-full">
+    <div class="max-w-[1600px] mx-auto">
+        <div
+            class="z-10 box-border mx-[2rem] relative flex justify-between items-center bg-black text-white rounded-lg px-12 py-7">
+            <div>
+                <div>Need more information?</div>
+                <div>Write your concern to us and our specialist will get back to you.</div>
+            </div>
+            <div class="w-fit">
+                <button type="button" class="bg-white text-black px-5 py-2">Contact Us</button>
+            </div>
+        </div>
+        <div class="flex justify-evenly bg-[#F8F8F8] px-10 pt-20 pb-16 mt-[-3rem]">
+            <div>
+                <div>Logo</div>
+                <div>
+                    <div>
+                        <div>Address</div>
+                        <div>Level 1, 12 Sample St, Sydney NSW 2000</div>
+                    </div>
+                    <div>
+                        <div>Contact</div>
+                        <div>1800 123 4567</div>
+                        <div>info@template.io</div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex gap-4">
+                <div>
+                    <div>Heading</div>
+                    <ul>
+                        <li>Link here</li>
+                        <li>Link here</li>
+                        <li>Link here</li>
+                        <li>Link here</li>
+                    </ul>
+                </div>
+                <div>
+                    <div>Heading</div>
+                    <ul>
+                        <li>Link here</li>
+                        <li>Link here</li>
+                        <li>Link here</li>
+                        <li>Link here</li>
+                    </ul>
+                </div>
+                <div>
+                    <div>Heading</div>
+                    <ul>
+                        <li>Link here</li>
+                        <li>Link here</li>
+                        <li>Link here</li>
+                        <li>Link here</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <div>Connect with us</div>
+                <div class="flex flex-col">
+                    <div>
+                        <img src="x.svg" alt="x">
+                    </div>
+                    <div>
+                        <img src="facebook.svg" alt="facebook">
+                    </div>
+                    <div>
+                        <img src="instagram.svg" alt="instagram">
+                    </div>
+                    <div>
+                        <img src="youtube.svg" alt="youtube">
+                    </div>
+                    <div>
+                        <img src="linkedin.svg" alt="linkedin">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
