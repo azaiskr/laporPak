3c9cc2391470905f8c7da929797d89d54b06e09d
Dhiya Urrahman <urrahmand0@gmail.com>
2024-09-11 14:21:06 +0700

    add admin dashboard
    create new admin dashboard, basic js implemented.

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,19 +1,18 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="px-12 py-6 flex flex-col gap-3">
-    <div class="w-full bg-white px-4 py-2">Dashboard</div>
-    <div class="w-full bg-white p-4">
-        <div class="flex gap-4">
-            <div class="w-full shadow p-4 rounded-2xl">
-                <div>Laporan Hari Ini</div>
-                <div>20</div>
-            </div>
-            <div class="w-full shadow p-4 rounded-2xl">
-                <div>Total Laporan</div>
-                <div>320</div>
-            </div>
+<div class="grid gap-8 md:grid-cols-2 lg:gap-12 p-6 md:p-10">
+    <a href="#" class="flex flex-col p-6 space-y-6 transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl lg:p-8 lg:flex-row lg:space-y-0 lg:space-x-6">
+        <div class="flex-1">
+            <h5 class="mb-3 text-xl font-bold lg:text-2xl">Laporan Hari Ini</h5>
+            <p class="mb-6 text-lg text-gray-600">20</p>
         </div>
-    </div>
+    </a>
+    <a href="#" class="flex flex-col p-6 space-y-6 transition-all duration-500 bg-white border border-indigo-100 rounded-lg shadow hover:shadow-xl lg:p-8 lg:flex-row lg:space-y-0 lg:space-x-6">
+        <div class="flex-1">
+            <h5 class="mb-3 text-xl font-bold lg:text-2xl">Total Laporan</h5>
+            <p class="mb-6 text-lg text-gray-600">320</p>
+        </div>
+    </a>
 </div>
 @endsection
