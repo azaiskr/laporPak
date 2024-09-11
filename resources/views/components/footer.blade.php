cf4f8e288d898610297e47e23a174ea2da513374
azaiskr <nuraziss557@gmail.com>
2024-09-11 23:37:53 +0700

    fix : home UI

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,63 +2,44 @@
     <div class="max-w-[1600px] mx-auto">
         <div class="z-10 box-border mx-40 relative flex justify-between items-center bg-black text-white rounded-lg px-12 py-7">
             <div>
-                <h2 class="text-4xl font-extrabold whitespace-nowrap mb-4">Need more information?</h2>
-                <div>Write your concern to us and our specialist will get back to you.</div>
+                <h2 class="text-4xl font-extrabold whitespace-nowrap mb-4">Butuh informasi lebih lanjut?</h2>
+                <div>Tulis kekhawatiran Anda kepada kami dan spesialis kami akan menghubungi Anda kembali.</div>
             </div>
-            <div class="w-fit text-center">
+            {{-- <div class="w-fit text-center">
                 <a href="#" class="inline-flex items-center justify-center px-8 py-4 text-2xl font-medium text-center text-black bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-100">
                     Button
                 </a> 
-            </div>
+            </div> --}}
         </div>
         <div>
-            <div class="flex justify-start items-start px-10 pt-20 pb-16 mt-[-3rem] mx-10 gap-14">
+            <div class="flex justify-start w-full items-start px-10 pt-20 pb-16 mt-[-3rem] mx-10 gap-14">
                 <div class="flex flex-col gap-8 sm:gap-6 sm:grid-row-3 text-black">
                     <div class="text-2xl font-extrabold whitespace-nowrap">
                         <span>Logo</span>
                     </div>
                     <div>
-                        <h2 class="text-md font-bold whitespace-nowrap">Address:</h2>
-                        <span>Level 1, 12 Sample St, Sydney NSW 2000</span>
+                        <h2 class="text-md font-bold whitespace-nowrap">Alamat:</h2>
+                        <span>...</span>
                     </div>
                     <div>
-                        <h2 class="text-md font-bold whitespace-nowrap">Contact:</h2>
-                        <p class="underline underline-offset-auto">1800 123 4567</p>
-                        <p class="underline underline-offset-auto">info@template.io</p>
+                        <h2 class="text-md font-bold whitespace-nowrap">Kontak:</h2>
+                        <p class="underline underline-offset-auto">+62</p>
+                        <p class="underline underline-offset-auto">...</p>
                     </div>
                 </div>
-                <div class="flex flex-row mt-4 justify-evenly mx-auto w-full">
-                    <div class="flex flex-row gap-32">
+                <div class="flex flex-row mt-4 justify-end mx-auto w-2/3">
+                    <div class="flex w-1/2">
                         <div>
-                            <h2 class="mb-6 text-xl font-semibold text-black">Heading</h2>
+                            <h2 class="mb-6 text-xl font-semibold text-black" >Site Map</h2>
                             <ul>
-                                <li>Link here</li>
-                                <li>Link here</li>
-                                <li>Link here</li>
-                                <li>Link here</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h2 class="mb-6 text-xl font-semibold text-black" >Heading</h2>
-                            <ul>
-                                <li>Link here</li>
-                                <li>Link here</li>
-                                <li>Link here</li>
-                                <li>Link here</li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h2 class="mb-6 text-xl font-semibold text-black" >Heading</h2>
-                            <ul>
-                                <li>Link here</li>
-                                <li>Link here</li>
-                                <li>Link here</li>
-                                <li>Link here</li>
+                                <li>Beranda</li>
+                                <li>Forum</li>
+                                <li>Lapor</li>
                             </ul>
                         </div>
                     </div>
                     <div>
-                        <h2 class="mb-6 text-xl font-semibold text-black" >Connect with us</h2>
+                        <h2 class="mb-6 text-xl font-semibold text-black" >Mari terbuhung dengan kami</h2>
                         <div class="flex flex-row gap-4">
                             <div>
                                 <img src="instagram.svg" alt="instagram">
