feba35bdfc97741461dff2344f5dcd09c6fde047
azaiskr <nuraziss557@gmail.com>
2024-09-12 10:31:08 +0700

    fix : bug middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,9 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
-        $middleware->use([
-            'CheckIsAdmin' => \App\Http\Middleware\CheckIsAdmin::class,
-        ]);
+        // $middleware->use([
+        //     \App\Http\Middleware\CheckIsAdmin::class,
+        // ]);
         
     })
     ->withExceptions(function (Exceptions $exceptions) {
